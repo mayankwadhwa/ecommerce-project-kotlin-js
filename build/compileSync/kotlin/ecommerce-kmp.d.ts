9b812f8cdc108ed08b1f6b3a726fda5cbd47b8f8
mayankwadhwa <mayankwadhwa@outlook.in>
2021-05-23 20:38:05 +0530

    unstaged build directory

--- build/compileSync/kotlin/ecommerce-kmp.d.ts
+++
@@ -1,6 +0,0 @@
-type Nullable<T> = T | null | undefined
-export class Welcome extends react.RComponent<WelcomeProps, WelcomeState> {
-    constructor(props: WelcomeProps);
-    render(_this_: react.RBuilder): void;
-}
-export as namespace ecommerce_kmp;
